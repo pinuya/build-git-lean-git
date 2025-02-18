94fb8a74641d3199c71c293f0f23481bc25141a1
Tifany <tifsnunes@gmail.com>
2025-02-18 15:14:33 -0300

    build my on git

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# Build Git - Learn Git
+# EN - Build Git - Learn Git in JavaScript
 
 What better way to learn Git than to implement one of your own!
 
-This is a very basic implementation of [Git version control system](http://git-scm.com/) by Linus Torvalds.
-(For educational purposes only)
+This is a very basic implementation of [Git version control system](http://git-scm.com/)
+
+# PT-BR - Construindo um Git - Aprendendo Git em JavaScript
+
+Que melhor maneira de aprender Git do que implementar o seu próprio!
+
+Esta é uma implementação muito básica de [Git version control system](http://git-scm.com/)
+Esse repositorio e para fins de estudos, utilizei o [CodeCrafts-io](https://github.com/codecrafters-io/build-your-own-x) para esse desafio.
--- git.js
+++ git.js
@@ -1,37 +1,108 @@
-function Git(name) {
-  this.name = name; // Repo name
-}
+(function () {
+  /**
+   * Commit classe
+   * Um único commit.
+   *
+   * @param {number} id  		 ID do commit.
+   * @param {Commit} parent	 Parent Commit.
+   * @param {string} msg 		 Mensagem de commit.
+   */
+  function Commit(id, parent, message) {
+    this.id = id;
+    this.parent = parent;
+    this.message = message;
+  }
 
-var repo = new Git("my-repo");
+  /**
+   * Branch class.
+   * A Branch.
+   * @param {string} name 	Nome da branch.
+   * @param {number} commit   Commit it points to.
+   */
+  function Branch(name, commit) {
+    this.name = name;
+    this.commit = commit;
+  }
 
-// Actual command:
-// > git init
+  /**
+   * Git classe
+   * Representa um repositório Git.
+   *
+   * @param {string} name Nome do repositorio.
+   */
+  function Git(name) {
+    this.name = name; // Repo name
+    this.lastCommitId = -1; // Acompanhe o último ID de commit.
+    this.branches = []; // Lista todas as branchs.
 
-function Commit(id) {
-  this.id = id;
-  // Assume that 'this' has a 'change' property too.
-}
+    var master = new Branch("master", null); // Nenhum commit ainda, então null foi passado.
+    this.branches.push(master); // Store master branch.
 
-function Commit(id, message) {
-  this.id = id;
-  this.message = message;
-}
+    this.HEAD = master; // HEAD aponta para o branch atual.
+  }
 
-Git.prototype.commit = function (message) {
-  var commit = new Commit();
-  return commit;
-};
+  /**
+   * Faca o commit.
+   * @param  {string} message Commit message.
+   * @return {Commit}        Objeto de commit criado.
+   */
+  Git.prototype.commit = function (message) {
+    // Incrementa o último ID de commit e passa para o novo commit.
+    var commit = new Commit(++this.lastCommitId, this.HEAD.commit, message);
 
-function Git() {
-  this.lastCommitId = -1;
-}
+    // Atualiza o ponteiro da branch atual para um novo commit.
+    this.HEAD.commit = commit;
 
-Git.prototype.commit = function (message) {
-  var commit = new Commit(++this.lastCommitId, message);
-  return commit;
-};
+    return commit;
+  };
 
-repo.commit("Make commit work");
+  /**
+   * Checkout da branch ou cria uma caso ela nao exista.
+   *
+   * @param  {string} branchName Branch para mudar ou criar
+   * @return {Branch}           Branch atual apos o checkout.
+   */
+  Git.prototype.checkout = function (branchName) {
+    // Faz um loop por todas as ramificações e vê se temos uma ramificação
+    // chamado `branchName`.
+    for (var i = this.branches.length; i--; ) {
+      if (this.branches[i].name === branchName) {
+        // Procura uma branch existente
+        console.log("Switched to existing branch: " + branchName);
+        this.HEAD = this.branches[i];
+        return this;
+      }
+    }
 
-// Actual command:
-// > git commit -m "Make commit work"
+    // Se a branch não for encontrada, crie uma nova.
+    var newBranch = new Branch(branchName, this.HEAD.commit);
+    // Store branch.
+    this.branches.push(newBranch);
+    // Atualiza o HEAD
+    this.HEAD = newBranch;
+
+    console.log("Switched to new branch: " + branchName);
+    return this;
+  };
+
+  /**
+   * Historico de Logs.
+   * @return {Array} Confirma em ordem cronológica inversa.
+   */
+  Git.prototype.log = function () {
+    // Comeca pelo HEAD do commit
+    var commit = this.HEAD.commit,
+      history = [];
+
+    while (commit) {
+      history.push(commit);
+      // Keep following the parent
+      commit = commit.parent;
+    }
+
+    return history;
+  };
+
+  // Exibe a classe Git na janela.
+  window.Git = Git;
+})();
